30c57c05723ea76f68fa3c943e0a6a974f509fe9
Devashish <devashish.86@gmail.com>
2018-04-26 13:27:01 +0530

    not updating pip

--- install.sh
+++ install.sh
@@ -109,7 +109,7 @@ echo "Installed Shippable CLIs successfully"
 echo "-------------------------------------"
 
 rm -rf /usr/local/lib/python2.7/dist-packages/requests*
-pip install --upgrade pip
+#pip install --upgrade pip
 
 echo "================== Installing python requirements ====="
 pip install -r /u16/requirements.txt

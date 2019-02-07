fa04157002c032421e78e436d5b96d887a74c264
Ragesh Krishna <ragesh@rageshkrishna.com>
2019-02-07 23:30:08 +0530

    Install ursa-wrapper globally

--- node/install.sh
+++ node/install.sh
@@ -6,3 +6,9 @@ wget https://nodejs.org/dist/"$NODE_VERSION"/node-"$NODE_VERSION"-linux-x64.tar.
 tar -xvf node-"$NODE_VERSION"-linux-x64.tar.xz
 cp -Rvf node-"$NODE_VERSION"-linux-x64/{bin,include,lib,share} /usr/local
 npm install -g forever@0.14.2 grunt grunt-cli
+
+# Install ursa-wrapper globally
+mkdir /tmp/ursa-wrapper
+cd /tmp/ursa-wrapper
+npm install ursa-wrapper@1.0.0
+mv node_modules/* /usr/local/lib/node_modules

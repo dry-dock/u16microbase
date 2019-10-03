1445246ab604ae02604be53fb754ed43537d58f9
Devashish <devashish.86@gmail.com>
2019-10-03 21:24:14 +0000

    updating base nodejs version

--- node/install.sh
+++ node/install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash -e
 
-NODE_VERSION=v8.11.3
+NODE_VERSION=v10.16.3
 echo "================= Installing nodejs $NODE_VERSION ================="
 wget https://nodejs.org/dist/"$NODE_VERSION"/node-"$NODE_VERSION"-linux-x64.tar.xz
 tar -xvf node-"$NODE_VERSION"-linux-x64.tar.xz
 cp -Rvf node-"$NODE_VERSION"-linux-x64/{bin,include,lib,share} /usr/local
 npm install -g forever@0.14.2 grunt grunt-cli
-
-# Install ursa-wrapper globally
-mkdir /tmp/ursa-wrapper
-cd /tmp/ursa-wrapper
-npm install ursa-wrapper@1.0.0
-mv node_modules/* /usr/local/lib/node_modules
